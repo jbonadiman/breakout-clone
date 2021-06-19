d52a8b010e35e68000514868feb2e4108c6174a4
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-19 13:18:08 -0300

    Gera vários blocos automaticamente;

--- src/entities.lua
+++ src/entities.lua
@@ -6,15 +6,22 @@ local ball = require('entities/ball')
 local brick = require('entities/brick')
 local pause_overlay = require('entities/pause_overlay')
 
-return {
+local entities = {
     boundary_bottom(400, 606),
     boundary_vertical(-6, 300),
     boundary_vertical(806, 300),
     boundary_top(400, -6),
     paddle(300, 500),
     ball(200, 200),
-    brick(100, 100),
-    brick(200, 100),
-    brick(300, 100),
     pause_overlay()
 }
+
+local row_width = love.window.getMode() - 20
+for number = 0, 38 do
+    local brick_x = ((number * 60) % row_width) + 40
+    local brick_y = (math.floor((number * 60) / row_width) * 40) + 80
+    entities[#entities + 1] = brick(brick_x, brick_y)
+end
+
+
+return entities
