21660235597eab1c14a5c10a4bb1ab6c2414c866
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-19 13:51:38 -0300

    Altera input para lidar com o módulo de estados criado;

--- src/input.lua
+++ src/input.lua
@@ -1,48 +1,46 @@
-local input = {}
-local press_functions = {}
-local release_functions = {}
+local state = require('state')
 
-input.left = false
-input.right = false
-input.paused = false
-input.press = function(pressed_key)
-    if press_functions[pressed_key] then
-        press_functions[pressed_key]()
+local press_functions = {
+    left = function()
+        state.button_left = true
+    end,
+    right = function()
+        state.button_right = true
+    end,
+    escape = function()
+        love.event.quit()
+    end,
+    space = function()
+        if state.game_over or state.stage_cleared then
+            return
+        end
+
+        state.paused = not state.paused
     end
-end
-
-input.release = function(released_key)
-    if release_functions[released_key] then
-        release_functions[released_key]()
+}
+local release_functions = {
+    left = function()
+        state.button_left = false
+    end,
+    right = function()
+        state.button_right = false
     end
-end
+}
 
-input.toggle_focus = function(focused)
-    if not focused then
-        input.paused = true
+return {
+    press = function(pressed_key)
+        if press_functions[pressed_key] then
+            press_functions[pressed_key]()
+        end
+    end,
+    release = function(released_key)
+        if release_functions[released_key] then
+            release_functions[released_key]()
+        end
+    end,
+    toggle_focus = function(focused)
+        if not focused then
+            state.paused = true
+        end
     end
-end
-
-
-press_functions.left = function()
-    input.left = true
-end
-press_functions.right = function()
-    input.right = true
-end
-press_functions.escape = function()
-    love.event.quit()
-end
-press_functions.space = function()
-    input.paused = not input.paused
-end
-
-
-release_functions.left = function()
-    input.left = false
-end
-release_functions.right = function()
-    input.right = false
-end
-
-return input
+}
