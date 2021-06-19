b7b7ce7e7eb9b8074ab05062b606489f9d2efc19
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-19 14:06:27 -0300

    Altera pause_overlay para usar novo módulo de estados;

--- src/entities/pause_overlay.lua
+++ src/entities/pause_overlay.lua
@@ -1,4 +1,4 @@
-local input = require('input')
+local state = require('state')
 
 return function()
     local window_width, window_height = love.window.getMode()
@@ -6,12 +6,12 @@ return function()
     local entity = {}
 
     entity.draw = function(self)
-        if input.paused then
-            love.graphics.setColor(1, 1, 1, 0.2)
+        if state.paused then
+            love.graphics.setColor({ 1, 1, 1, 0.2 })
             love.graphics.rectangle('fill', 0, 0, window_width, window_height)
-            love.graphics.setColor(1, 1, 1, 1)
+            love.graphics.setColor(state.palette[5])
             love.graphics.print(
-                {{ 0.2, 1, 0.2, 1 }, 'PAUSED'},
+                { state.palette[3], 'PAUSED' },
                 math.floor(window_width / 2) - 54,
                 math.floor(window_height / 2),
                 0,
