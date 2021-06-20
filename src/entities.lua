7f7a48a05a6bc770261fa4445660054090e22e9d
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-20 01:27:38 -0300

    Reimplementa ball como uma classe;

--- src/entities.lua
+++ src/entities.lua
@@ -2,7 +2,7 @@ local boundary_bottom = require('entities/boundary_bottom')
 local boundary_vertical = require('entities/boundary_vertical')
 local boundary_top = require('entities/boundary_top')
 local paddle = require('entities/paddle')
-local ball = require('entities/ball')
+local Ball = require 'entities/ball'
 local brick = require('entities/brick')
 local pause_overlay = require('entities/pause_overlay')
 local game_over_text = require('entities/game_over_text')
@@ -15,7 +15,7 @@ local entities = {
     boundary_vertical(806, 300),
     boundary_top(400, -6),
     paddle(300, 500),
-    ball(200, 200),
+    Ball(200, 200),
     pause_overlay(),
     game_over_text(),
     stage_clear_text(),
--- src/entities/ball.lua
+++ src/entities/ball.lua
@@ -1,44 +1,48 @@
-local world = require('world')
+local world = require 'world'
+local Object = require 'classic'
 
-return function(x, y)
-    local entity_max_speed = 880
+Ball = Object:extend()
+Ball.max_speed = 880
 
-    local entity = {}
-    entity.body = love.physics.newBody(world, x, y, 'dynamic')
-    entity.body:setMass(32)
-    entity.body:setLinearVelocity(300, 300)
-    entity.shape = love.physics.newCircleShape(0, 0, 10)
-    entity.fixture = love.physics.newFixture(entity.body, entity.shape)
-    entity.fixture:setRestitution(1)
-    entity.fixture:setFriction(0)
-    entity.fixture:setUserData(entity)
-
-    entity.draw = function(self)
-        local self_x, self_y = self.body:getWorldCenter()
-        love.graphics.circle(
-            'fill',
-            self_x,
-            self_y,
-            self.shape:getRadius()
-        )
-    end
-
-    entity.update = function(self)
-        local vel_x, vel_y = self.body:getLinearVelocity()
-        local speed = math.abs(vel_x) + math.abs(vel_y)
-
-        local vel_x_is_critical = math.abs(vel_x) > entity_max_speed * 2
-        local vel_y_is_critical = math.abs(vel_y) > entity_max_speed * 2
-
-        if vel_x_is_critical or vel_y_is_critical then
-            self.body:setLinearVelocity(vel_x * .75, vel_y * .75)
-        end
-        if speed > entity_max_speed then
-            self.body:setLinearDamping(0.1)
-        else
-            self.body:setLinearDamping(0)
-        end
-    end
-
-    return entity
+function Ball:new(x, y)
+    self.x = x or 0
+    self.y = y or 0
+    self.body = love.physics.newBody(world, self.x, self.y, 'dynamic')
+    self.body:setMass(32)
+    self.body:setLinearVelocity(300, 300)
+    self.shape = love.physics.newCircleShape(0, 0, 10)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+    self.fixture:setRestitution(1)
+    self.fixture:setFriction(0)
+    self.fixture:setUserData(self)
 end
+
+function Ball:draw()
+    local x, y = self.body:getWorldCenter()
+    love.graphics.circle(
+        'fill',
+        x,
+        y,
+        self.shape:getRadius()
+    )
+end
+
+function Ball:update()
+    local vel_x, vel_y = self.body:getLinearVelocity()
+    local speed = math.abs(vel_x) + math.abs(vel_y)
+
+    local vel_x_is_critical = math.abs(vel_x) > Ball.max_speed * 2
+    local vel_y_is_critical = math.abs(vel_y) > Ball.max_speed * 2
+
+    if vel_x_is_critical or vel_y_is_critical then
+        self.body:setLinearVelocity(vel_x * .75, vel_y * .75)
+    end
+
+    if speed > Ball.max_speed then
+        self.body:setLinearDamping(0.1)
+    else
+        self.body:setLinearDamping(0)
+    end
+end
+
+return Ball
