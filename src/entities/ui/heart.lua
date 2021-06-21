dc7e09fb27d1ecf2b4b3c93e3cb109901fce74f2
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-21 00:12:26 -0300

    WIP vidas como classe;

--- src/entities/ui/heart.lua
+++ src/entities/ui/heart.lua
@@ -1,5 +1,55 @@
 local state = require('state')
 
+local Object = require 'classic'
+
+Lives = Object:extend()
+
+function Lives:load()
+    Heart.load()
+end
+
+function Lives:new(x, y)
+    self.x = x or 0
+    self.y = y or 0
+
+    for i = 1, state.lives do
+        -- spawn hearts
+    end
+end
+
+function Lives:draw()
+    for i = 1, state.lives do
+        love.graphics.draw(
+            self.sprite,
+            self.width * (i - 1) + 8,
+            16
+        )
+    end
+end
+
+Heart = Object:extend()
+
+function Heart.load()
+    Heart.sprite = love.graphics.newImage("spr/heart.png")
+    Heart.width = Heart.sprite:getWidth()
+    Heart.height = Heart.sprite:getHeight()
+end
+
+function Heart:new(x, y)
+    self.x = x or 0
+    self.y = y or 0
+end
+
+function Heart:draw()
+    love.graphics.draw(
+        Heart.sprite,
+        self.x,
+        self.y
+    )
+end
+
+
+
 return function()
     local entity = {}
 
