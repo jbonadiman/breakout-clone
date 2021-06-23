0a29ba3d323314cb922d1fa8ecc2fdbfc6ac5fde
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-22 21:16:38 -0300

    Move entidades de interface para a pasta apropriada;

---
+++ src/entities/ui/pause_overlay.lua
@@ -0,0 +1,31 @@
+local state = require 'state'
+local Object = require 'classic'
+
+PauseOverlay = Object:extend()
+local window_width, window_height = love.window.getMode()
+
+function PauseOverlay.load()
+    PauseOverlay.font = love.graphics.newFont(64)
+    PauseOverlay.text = love.graphics.newText(
+        PauseOverlay.font,
+        { state.palette[3], 'PAUSED' }
+    )
+
+    PauseOverlay.x = math.floor(window_width / 2 - PauseOverlay.text:getWidth() / 2)
+    PauseOverlay.y = math.floor(window_height / 2 - PauseOverlay.text:getHeight() / 2)
+end
+
+function PauseOverlay:draw()
+    if state.paused then
+        love.graphics.setColor({ 1, 1, 1, 1 })
+        love.graphics.rectangle('fill', 0, 0, window_width, window_height)
+
+        love.graphics.draw(
+            PauseOverlay.text,
+            PauseOverlay.x,
+            PauseOverlay.y
+        )
+    end
+end
+
+return PauseOverlay
