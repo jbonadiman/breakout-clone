221115de4d9a551774de4c7354c781a420713643
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-22 21:24:31 -0300

    Implementa textos na tela como fonts e texts;

--- src/entities/ui/game_over_text.lua
+++ src/entities/ui/game_over_text.lua
@@ -5,15 +5,23 @@ local window_width, window_height = love.window.getMode()
 
 GameOverText = Object:extend()
 
+function GameOverText.load()
+    GameOverText.font = love.graphics.newFont(64)
+    GameOverText.text = love.graphics.newText(
+        GameOverText.font,
+        { state.palette[5], 'GAME OVER' }
+    )
+
+    GameOverText.x = math.floor(window_width / 2 - GameOverText.text:getWidth() / 2)
+    GameOverText.y = math.floor(window_height / 2 - GameOverText.text:getHeight() / 2)
+end
+
 function GameOverText:draw()
     if state.game_over then
-        love.graphics.print(
-            { state.palette[5], 'GAME OVER' },
-            math.floor(window_width / 2) - 100,
-            math.floor(window_height / 2),
-            0,
-            2,
-            2
+        love.graphics.draw(
+            GameOverText.text,
+            GameOverText.x,
+            GameOverText.y
         )
     end
 end
--- src/entities/ui/stage_clear_text.lua
+++ src/entities/ui/stage_clear_text.lua
@@ -4,15 +4,23 @@ local Object = require 'classic'
 local window_width, window_height = love.window.getMode()
 StageClearText = Object:extend()
 
+function StageClearText.load()
+    StageClearText.font = love.graphics.newFont(64)
+    StageClearText.text = love.graphics.newText(
+        StageClearText.font,
+        { state.palette[4], 'STAGE CLEARED' }
+    )
+
+    StageClearText.x = math.floor(window_width / 2 - StageClearText.text:getWidth() / 2)
+    StageClearText.y = math.floor(window_height / 2 - StageClearText.text:getHeight() / 2)
+end
+
 function StageClearText:draw()
     if state.stage_cleared then
-        love.graphics.print(
-            { state.palette[4], 'STAGE CLEARED' },
-            math.floor(window_width / 2) - 110,
-            math.floor(window_height / 2),
-            0,
-            2,
-            2
+        love.graphics.draw(
+            StageClearText.text,
+            StageClearText.x,
+            StageClearText.y
         )
     end
 end
