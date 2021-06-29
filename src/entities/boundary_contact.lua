37a3f18cd0685fc84fef3072597eaa4c54ca64f2
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-29 00:41:53 -0300

    Corrige bug em que o jogo acaba com uma vida na tela;

--- src/entities/boundary_contact.lua
+++ src/entities/boundary_contact.lua
@@ -7,9 +7,12 @@ function BoundaryContact:new(x, y, width, height)
     BoundaryContact.super.new(self, x, y, width, height)
 end
 
+function BoundaryContact:update()
+    state.gameOver = state.lives < 1
+end
+
 function BoundaryContact:endContact()
     state.lives = state.lives - 1
-    state.gameOver = state.lives < 1
 end
 
 return BoundaryContact
